06e0d763348b202d4826230e71513ac11fd127b1
Rubem <rfsv@ic.ufal.br>
2020-08-28 21:46:54 -0300

    Connect with the database

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
+import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+
+import './database';
 
 const app = express();
 
@@ -7,5 +10,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!')
-})
+  console.log('🚀 Server started on port 3333!');
+});
