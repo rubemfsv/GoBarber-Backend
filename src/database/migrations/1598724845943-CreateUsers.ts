b55f2ec62bd29eb7d02d8d626a63d4b18cf7213f
Rubem <rfsv@ic.ufal.br>
2020-08-29 15:27:24 -0300

    Add model and migration for users

--- src/database/migrations/1598724845943-CreateUsers.ts
+++ src/database/migrations/1598724845943-CreateUsers.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1598624161608 implements MigrationInterface {
+export default class CreateUsers1598724845943 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -14,13 +14,19 @@ export default class CreateAppointments1598624161608 implements MigrationInterfa
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
             isNullable: false,
           },
           {
@@ -39,6 +45,6 @@ export default class CreateAppointments1598624161608 implements MigrationInterfa
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
