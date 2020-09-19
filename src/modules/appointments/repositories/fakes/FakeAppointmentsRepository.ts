8a8753159414a61a31f9d4aaf202c21e8609e945
Rubem <rfsv@ic.ufal.br>
2020-09-19 19:39:00 -0300

    Coverage report added

---
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,31 @@
+import { v4 } from 'uuid';
+
+import Appointment from '../../infra/typeorm/entities/Appointment';
+
+import IAppointmentsRepository from '../IAppointmentsRepository';
+import ICreateAppointmentDTO from '../../dtos/ICreateAppointmentDTO';
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find((appointment) => appointment.date === date);
+
+    return findAppointment;
+  }
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: v4(), date, provider_id });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
