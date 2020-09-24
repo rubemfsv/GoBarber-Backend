d3d9348f91777eebf8dd8248d7eda31cf7273f8f
Rubem <rfsv@ic.ufal.br>
2020-09-24 16:37:12 -0300

    Express rate limit added

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -8,6 +8,7 @@ import 'express-async-errors';
 
 import routes from './routes';
 import uploadConfig from '../../../config/upload';
+import rateLimiter from './middlewares/rateLimiter';
 import AppError from '../../errors/AppError';
 
 import '../typeorm';
@@ -15,6 +16,7 @@ import '../../container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
